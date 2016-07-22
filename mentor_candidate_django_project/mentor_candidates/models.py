380cf03ea9750d40875566b792861c5f1e6030bf
traffii <trafgraf@gmail.com>
2016-07-22 12:16:51 +0200

    Add Mentor model

--- mentor_candidate_django_project/mentor_candidates/models.py
+++ mentor_candidate_django_project/mentor_candidates/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Mentor(models.Model):
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
+    description = models.TextField()
+
