ac4bccad56382f39a465d717c99957fe9c164ddf
traffii <trafgraf@gmail.com>
2016-07-22 12:29:21 +0200

    Change email filed from CharField to EmailField

--- mentor_candidate_django_project/mentor_candidates/models.py
+++ mentor_candidate_django_project/mentor_candidates/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Mentor(models.Model):
     first_name = models.CharField(max_length=70)
     last_name = models.CharField(max_length=100)
-    email = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
     description = models.TextField(blank=True)
 
 
