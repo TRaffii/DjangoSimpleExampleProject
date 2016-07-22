5a2661a20fca060aad354fa3db44c4eb6b150693
traffii <trafgraf@gmail.com>
2016-07-22 13:29:41 +0200

    Add default method of object parsing to string for Mentor and Opinion

--- mentor_candidate_django_project/mentor_candidates/models.py
+++ mentor_candidate_django_project/mentor_candidates/models.py
@@ -9,9 +9,13 @@ class Mentor(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=200)
     description = models.TextField(blank=True)
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 
 class Opinion(models.Model):
     mentor = models.ForeignKey(Mentor)
     name = models.CharField(max_length=300)
     description = models.TextField()
+    def __str__(self):
+        return self.name
